5cabca888a2911758c7d241f908a169fda8e3e5c
aming103 <aming103@gmail.com>
2020-08-19 17:23:27 +0800

    Update RightClick.js
    1. 完成右鍵根據客戶SPEC比對

--- public/js/RightClick/RightClick.js
+++ public/js/RightClick/RightClick.js
@@ -38,15 +38,21 @@ function showSelectSPEC()
         },
         success: function (DownLoadValue){
             var data = DownLoadValue.success;
-    
+            sessionStorage.setItem('CustomerItem' , JSON.stringify(DownLoadValue.success));
+            
             //產生要寫入excel的data
             var table = "import_preview";
             var pcontent = '<span style="font-weight:bold; color:#2e6e9e;">《 產品規格 ProductSPEC 》</span><br /><br />'
             + '<div id="jqxcombobox_SPEC" ></div>' 
             + '<div id="jqxToolBar_SPEC" style = margin:0px auto; text-align:justify ></div>'
+            + '</br>'
+            + '<table id= "ProductSPEC"></table>'
+            + '<div id= "ProductSPECPager"></div>'
+            + '</br>'
             + '<table id= '+ table + '></table>';
            
-                
+           
+            
             //建立動態表格
             $("#confirmDialog").html(pcontent);
         
@@ -177,7 +183,7 @@ function ShowTableDynamic(label)
                     source.push(i);
                 }
             }
-            sessionStorage.setItem('CustomerSPEC' , source);
+            sessionStorage.setItem('CustomerSPEC' , JSON.stringify(DownLoadValue.product_SPEC));
             CreateToolBar(source);
         }                                   
     });  
@@ -185,7 +191,6 @@ function ShowTableDynamic(label)
 
 function CreateToolBar(source)
 {
-    //var data = sessionStorage.getItem('source');
     $("#jqxToolBar_SPEC").jqxToolBar("destroyTool", 1 );
     $("#jqxToolBar_SPEC").jqxToolBar('render');
 
@@ -220,9 +225,154 @@ function CreateToolBar(source)
         tool.on("change", function (event) {          
             var args = event.args;
                 if (args) {
-                    var position = "last";                                                     
+                    var label = args.item["label"]
+                    var data =  JSON.parse(sessionStorage.getItem('CustomerSPEC'));
+                    var _compare = [] ;  //獲得SPEC資料
+                    for(var i in data)
+                    {
+                        var tmp = {
+                            ELEMENT : data[i]["ELEMENT"],
+                            SPEC : data[i][label],
+                        }
+                        _compare.push (tmp)
+
+                    }
+
+                    CompareItemWithSPEC(_compare);
                 }
             });
     
     });
+}
+
+function CompareItemWithSPEC(_compare) 
+{
+    sessionStorage.setItem('_CompareSource', JSON.stringify(_compare));
+    var data =  JSON.parse(sessionStorage.getItem('CustomerItem'));
+    var table = "import_preview";
+    var colNames = [];
+    var colModel = [];
+    
+    $("#import_preview").jqGrid('GridUnload');
+    $("#ProductSPEC").jqGrid('GridUnload'); 
+
+    for ( var colName in data[0])
+    {
+        colNames.push(getColumnNameFromDatabaseToChinese(colName));
+    }
+
+    for ( var colName in data[0])
+    {           
+        if (colName === 'id')
+        {
+            colModel.push({name:colName, index:colName, align:"center", width:84, frozen:true, sortable:true, sorttype:"int"});
+        }
+        else
+        {
+            colModel.push({name:colName, index:colName, align:"center", width:112, cellattr: compareCellAttr});
+        }
+    }
+    
+    $("#" + table).jqGrid({      
+        datatype: "local",
+        data:data,
+        colNames: colNames,
+        colModel: colModel,
+        width: 896,
+        height: 'auto',
+        sortname: 'id',
+        sortorder: "asc",
+        hidegrid: false,
+        cmTemplate: { title: false },   // Hide Tooltip
+        gridview: true,
+        shrinkToFit: false,
+        rowNum:10,
+        rowList:[10,20,50],
+        pager: '#import_previewPager',
+        caption: "產品 Product", 
+        loadComplete: function (){ fixPositionsOfFrozenDivs.call(this); }, // Fix column's height are different after enable frozen column feature 
+        gridComplete: function() { $("#" + table).jqGrid('setFrozenColumns');}
+    });
+
+    var LineData = [];
+    var LineKey = [];
+    var LineValue = [];
+    var json = { };
+    for(var tmp in _compare)
+    {
+        LineKey.push(_compare[tmp]["ELEMENT"]);
+        LineValue.push(_compare[tmp]["SPEC"]);
+    }
+    for(var i = 0, l = LineKey.length; i < l; i++) {
+        json[LineKey[i]] = LineValue[i];
+    }
+
+    LineData.push(json);
+
+    var colNamesSPEC = [];
+    var colModelSPEC = [];
+    for ( var colName in  LineData[0])
+    {
+        colNamesSPEC.push(getColumnNameFromDatabaseToChinese(colName));
+    }
+
+    for ( var colName in  LineData[0])
+    {           
+        if (colName === 'id')
+        {
+            colModelSPEC.push({name:colName, index:colName, align:"center", width:84, frozen:false, sortable:true, sorttype:"int"});
+        }
+        else
+        {
+            colModelSPEC.push({name:colName, index:colName, align:"center", width:112});
+        }
+    }
+    
+    $("#ProductSPEC").jqGrid({      
+        datatype: "local",
+        data: LineData,
+        colNames: colNamesSPEC,
+        colModel: colModelSPEC,
+        width: 896,
+        height: 'auto',
+        sortname: 'id',
+        sortorder: "asc",
+        hidegrid: false,
+        cmTemplate: { title: false },   // Hide Tooltip
+        gridview: true,
+        shrinkToFit: false,
+        rowNum:10,
+        rowList:[10,20,50],
+        pager:'#ProductSPECPager',
+        caption: "客戶 SPEC", 
+        loadComplete: function (){ fixPositionsOfFrozenDivs.call(this); }, // Fix column's height are different after enable frozen column feature 
+        gridComplete: function() { $("#" + table).jqGrid('setFrozenColumns');}
+    });
+
+    
+}
+
+function compareCellAttr(rowId, val, rawObject, cm, rdata)
+{
+    var data = JSON.parse(sessionStorage.getItem('_CompareSource'));
+    var product = '';
+    var product_level = '';
+    var cloumnName = getColumnNameFromDatabaseToChinese(cm["name"]);
+    var sty = "style='font-size:14px'";
+
+    var value = val.split("<");
+            
+    val = value[value.length - 1];
+   
+    for(var j in data)
+    {
+        var _productS = data[j]["SPEC"];
+        if ( data[j]["ELEMENT"] == cloumnName &&  (parseFloat(_productS) <  parseFloat(val)) && _productS != '-')
+        {
+            sty = "style='font-size:14px; background-color:#9dbcfa'" ;
+        }
+    }
+
+    return sty;
+
 }
