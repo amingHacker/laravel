16cf96255825ed700ad4671d8cf126d9ac41f36d
aming103 <aming103@gmail.com>
2020-08-25 09:34:18 +0800

    sampling record column rearrange

--- database/migrations/2020_08_25_092342_add_sampling_records_table.php
+++ database/migrations/2020_08_25_092342_add_sampling_records_table.php
@@ -14,7 +14,7 @@ class AddSamplingRecordsTable extends Migration
     public function up()
     {
         Schema::table('sampling_records', function (Blueprint $table) {
-            $table->string("equipment_name", 100)->after('IR_A');
+            $table->string("equipment_name", 100)->after('batch_number');
             $table->string("standard_solution", 100)->after('equipment_name');
             $table->string("sampling_kind", 100)->after('standard_solution');
         });
